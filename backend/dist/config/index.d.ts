cea70e934260746027ec7e59a8c763326c811337
Santiago Cardona <santiagocardona97@gmail.com>
2025-10-07 09:41:42 -0500

    Add backend as normal directory

---
+++ backend/dist/config/index.d.ts
@@ -0,0 +1,14 @@
+declare const config: {
+    port: string | number;
+    smtp: {
+        host: string;
+        port: number;
+        user: string;
+        pass: string;
+    };
+    fromEmail: string;
+    toEmail: string;
+    frontendUrl: string;
+};
+export default config;
+//# sourceMappingURL=index.d.ts.map
---
+++ backend/dist/controllers/contactController.d.ts
@@ -0,0 +1,3 @@
+import { Request, Response } from "express";
+export declare function postContact(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
+//# sourceMappingURL=contactController.d.ts.map
